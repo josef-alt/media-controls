c89df20bffa52d57215df32a2525074953c4bb15
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-12-30 17:05:22 -0600

    created constants for repeatedly accessed attributes

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-console.log('file opened');
-
-	console.log('loaded');
-	
 	// TODO find media element
-	
-	// observe resizing
-	const observer = new ResizeObserver(() => {
-		console.log('resized');
-		
-		// TODO handle video resizing
-	});
-	
-	observer.observe(document.documentElement);
+// constant used for easy access to width attribute
+const docElement = document.documentElement;
+
+// find media element
+const video = document.querySelector('video');
+
+// constant used for scaling media elements
+const SCALE_FACTOR = 0.85;
+
+observer.observe(docElement);
