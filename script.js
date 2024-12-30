16835b66cdab15672128db401585d2fb4fbc8e6c
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-12-30 16:52:59 -0600

    setup resize detection

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('file opened');
+
+// wait for file to load
+document.addEventListener("DOMContentLoaded", function() {
+	console.log('loaded');
+	
+	// TODO find media element
+	
+	// observe resizing
+	const observer = new ResizeObserver(() => {
+		console.log('resized');
+		
+		// TODO handle video resizing
+	});
+	
+	observer.observe(document.documentElement);
+});
