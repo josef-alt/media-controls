0c71aae97a115af135582a251704e59f40916d94
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-12-30 17:39:10 -0600

    extracted logic to functions and added null check to prevent unnecessary execution

--- script.js
+++ script.js
@@ -7,36 +7,48 @@ const video = document.querySelector('video');
 // constant used for scaling media elements
 const SCALE_FACTOR = 0.85;
 
-// observe resizing
-const observer = new ResizeObserver(() => {
-	// apply scale factor to new window size
-	const newWidth = SCALE_FACTOR * docElement.clientWidth;
-	video.width = newWidth;
-});
+// setup resizing
+function reactiveResize() {
+	// observe resizing
+	const observer = new ResizeObserver(() => {
+		// apply scale factor to new window size
+		const newWidth = SCALE_FACTOR * docElement.clientWidth;
+		video.width = newWidth;
+	});
 
-observer.observe(docElement);
+	observer.observe(docElement);
+}
 
-// adding keyboard controls
-document.addEventListener('keydown', function(event) {
-	switch(event.key) {
-		case 'ArrowLeft':
-			video.currentTime -= 5;
-			break;
-		case 'ArrowRight':
-			video.currentTime += 5;
-			break;
-		case 'j':
-			video.currentTime -= 10;
-			break;
-		case 'l':
-			video.currentTime += 10;
-			break;
-		case 'k':
-			if(video.paused) {
-				video.play();
-			} else {
-				video.pause();
-			}
-			break;
-	}
-});
+// setup key-binds
+function bindControls() {
+	// adding keyboard controls
+	document.addEventListener('keydown', function(event) {
+		switch(event.key) {
+			case 'ArrowLeft':
+				video.currentTime -= 5;
+				break;
+			case 'ArrowRight':
+				video.currentTime += 5;
+				break;
+			case 'j':
+				video.currentTime -= 10;
+				break;
+			case 'l':
+				video.currentTime += 10;
+				break;
+			case 'k':
+				if(video.paused) {
+					video.play();
+				} else {
+					video.pause();
+				}
+				break;
+		}
+	});
+}
+
+// make sure the focused file is a video before trying to modify it
+if(video) {
+	reactiveResize();
+	bindControls();
+}
